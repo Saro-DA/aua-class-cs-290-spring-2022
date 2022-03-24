5517c5cb3597254d9fb4ad3c168faa62631d857f
Saro Deravanesian <saro.deravanesian@gmail.com>
2022-03-24 12:41:40 +0400

    Added password hashing.

--- express/students/entities/student.enitity.js
+++ express/students/entities/student.enitity.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const studentSchema = new mongoose.Schema({
     firstName: String,
@@ -6,5 +7,12 @@ const studentSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
+studentSchema.pre('save', function () {
+    if (this.isModified('password')) {
+        const salt = bcrypt.genSaltSync(10);
+        this.password = bcrypt.hashSync(this.password, salt);
+    }
+})
 
 module.exports = mongoose.model('Student', studentSchema);
