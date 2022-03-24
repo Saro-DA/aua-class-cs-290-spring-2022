9ad00e97a6d4283d5e8272231128288f4451e817
Saro Deravanesian <saro.deravanesian@gmail.com>
2022-03-24 12:28:10 +0400

    Constraints on Student collection.

--- express/students/entities/student.enitity.js
+++ express/students/entities/student.enitity.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
     firstName: String,
-    lastName: String
+    lastName: String,
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Student', studentSchema);
